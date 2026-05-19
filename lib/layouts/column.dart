5f44280b70bf912299ba05fa6153b2427f3e7dcb
JoelCrDp <=>
2026-05-18 23:47:11 -0400

    Desde casa

--- lib/layouts/column.dart
+++ lib/layouts/column.dart
@@ -8,19 +8,22 @@ class ColumExample extends StatelessWidget {
   Widget build(BuildContext context) {
     //Permite poner vistas una debajo de otra
     return Container(
-      //El container es otro componente que tambie ntiene muchos atributos, es el padre de la columna y sus cambios seran hacia el
-      width: 1000,
+      //El container es otro componente que tambien tiene muchos atributos, es el padre de la columna y sus cambios seran hacia el
+      width: double.infinity, //Dice que ocupe todo el ancho sin iomportar que la columna lo nesesite
+      //width: 1000,
       //height: 800,
-      color: const Color.fromARGB(255, 186, 180, 163),
+      color: const Color.fromARGB(255, 38, 69, 172), //Color de fondo
       child: Column(
         //Se crea un columna para añadir widgets como un div
         mainAxisAlignment: MainAxisAlignment
-            .center, //Metodo para alineamientos de widgets (INVESTIGAR EL MODULO)
+            .center, //Metodo para alineamientos de widgets (INVESTIGAR EL MODULO) alinea en vertical
         mainAxisSize: MainAxisSize
             .max, //Tamaño del eje principal nos die caunto de largo ocuapra la columna
+        crossAxisAlignment: CrossAxisAlignment
+            .center, //A comparacion del alignment este alinea en horizontal
         children: [
           Text(
-            "Columna 1111111111111111111111111111111111111111111",
+            "Columna 1",
           ), //Estos componentes ya son filas dentro de la columna
           Text("Columna 1"),
           Text("Columna 1"),
